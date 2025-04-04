60472e7141cce94baf9b7ca7d41d8af991456ca5
unknown <blackskiesdancer@gmail.com>
2025-04-04 22:52:53 +1100

    sparkle mouse & shadow-drop

--- index.js
+++ index.js
@@ -12,3 +12,16 @@
 
  updateClock()
  setInterval(updateClock, 1000)
+
+ 
+ document.addEventListener('mousemove', (e) => {
+   const sparkle = document.createElement('div');
+   sparkle.className = 'sparkle';
+   sparkle.style.left = `${e.pageX}px`;
+   sparkle.style.top = `${e.pageY}px`;
+   document.body.appendChild(sparkle);
+   
+   setTimeout(() => {
+     sparkle.remove();
+   }, 800);
+ });
